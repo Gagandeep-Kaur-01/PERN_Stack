d6dca839c9997a71c8d9ed5554ee18faba821044
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 13:12:35 +0530

    worked with post request

--- server/index.js
+++ server/index.js
@@ -5,15 +5,21 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json()); // to access json data
+app.use(express.json()); //req.body
 
 // ------- Routes ------ //
 
 
 // create a todo
-app.post("/todos", async(res, req) => {
+app.post("/todos", async(req, res) => {
     try {
-        console.log(req.body);
+        const { description } = req.body;
+        const newTodo = await pool.query (
+            "INSERT INTO todo (description) VALUES($1)",
+             [description]
+        );
+
+        res.json(newTodo);
     }  
     catch (err) {
         console.error(err.message);
