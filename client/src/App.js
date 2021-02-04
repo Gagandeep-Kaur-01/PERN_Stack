381c29a39b9b2400aa7322732119102b170e2b68
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 15:55:13 +0530

    imported component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
 
 function App() {
   return (
     <Fragment>
       <div className="container">        
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   );
