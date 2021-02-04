e862d07f858eb55ab0795dc96961907e5c690fbd
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 16:04:00 +0530

    created table and displayed hard coded data

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const ListTodos = () => {
     return (
-      <h1>
-        List Todos
-      </h1>
+        <Fragment>
+          {" "}
+          <table className="table mt-5 text-center">
+           <thead>
+            <tr>
+              <th>Description</th>
+             <th>Edit</th>
+             <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+              {/* */}
+            <tr>
+              <td>.......</td>
+              <td>button</td>
+              <td>button</td>
+            </tr>      
+          </tbody>
+         </table>
+      </Fragment>
     )
 };
 
