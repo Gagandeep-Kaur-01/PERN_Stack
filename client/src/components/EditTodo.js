81a75d7b6768a8722e3f97a3eb867122ddbdd259
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 16:41:30 +0530

    inserted fragment component

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const EditTodo = () => {
     return (
-        <h1>
-            Edit Todo
-        </h1>
+        <Fragment>
+            
+        </Fragment>
     )
 }
 
