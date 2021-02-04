f3adf4364cdd13e92546830ccceb6c6659263f09
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 15:34:47 +0530

    created form

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -4,6 +4,10 @@ const InputTodo = () => {
     return (
         <Fragment>
           <h1 className="text-center mt-5"> Input Todo </h1> 
+          <form className="d-flex mt-5">
+              <input type="text" className="form-control" />
+              <button className="btn btn-success">Add</button>
+          </form>
         </Fragment>
     )
 }
