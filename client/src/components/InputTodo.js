00698079b566fd1dd9464c7c5574cd7658e1c7a2
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 15:37:22 +0530

    passed initial value

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -2,13 +2,13 @@ import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
 
-    const [description, setDescription] = useState("");
-    
+    const [description, setDescription] = useState("initial value");
+
     return (
         <Fragment>
           <h1 className="text-center mt-5"> Input Todo </h1> 
           <form className="d-flex mt-5">
-              <input type="text" className="form-control" />
+              <input type="text" className="form-control" value={description} />
               <button className="btn btn-success">Add</button>
           </form>
         </Fragment>
