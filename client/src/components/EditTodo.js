1d39f6b2004052cfb55073fc04b9da7003ef3d30
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 17:12:05 +0530

    edited the data

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -5,9 +5,17 @@ const EditTodo = ({ todo }) => {
     const [description, setDescription] = useState(todo.description);
 
     //edit description function
-    const updateDescription = async() => {
+    const updateDescription = async(e) => {
+        e.preventDefault();
         try{
+            const body = { description };
+            const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            });
 
+            console.log(response);
         } catch(err) {
             console.error(err.message)
         }
