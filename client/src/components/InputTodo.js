1b98a936ce517c07b2f5a2bdb71946eecce7f2f0
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-04 15:49:19 +0530

    building input todo component, used post method

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -4,10 +4,26 @@ const InputTodo = () => {
 
     const [description, setDescription] = useState("");
 
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        try {
+            const body = {description};
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+                headers: {"Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+
+            console.log(response);
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
     return (
         <Fragment>
           <h1 className="text-center mt-5"> Input Todo </h1> 
-          <form className="d-flex mt-5">
+          <form className="d-flex mt-5" onSubmit={onSubmitForm} >
               <input 
                 type="text" 
                 className="form-control" 
